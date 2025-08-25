a39abc29daa763a83b039d1f9321550b197274b2
Peter Koopman <peter@scribbledesign.co.nz>
2025-08-25 16:18:28 +1200

    Tidy up

--- src/server.js
+++ src/server.js
@@ -41,10 +41,10 @@ app.get('/setup', async (req, res) => {
     await pool.query(qryGames);
     await pool.query(qryPlayers);
     await pool.query(addGames);
-    return res.sendStatus(200);
+    return res.status(200).send('Successful setup');
   } catch(error) {
     console.log(error);
-    res.sendStatus(500);
+    res.status(500).send(`Error: could not setup database tables. ${error}`);
   }
 
 });
@@ -53,15 +53,17 @@ app.get('/setup', async (req, res) => {
 
 app.post('/games', async (req, res) => {
   const {title, date, location, description} = req.body;
-  const qry = 'INSERT INTO games (title, date, location, description) VALUES ($1, $2, $3, $4)';
+  if(!title || !date || !location || !description) {
+    return res.status(400).send('Missing required fields');
+  }
+  const qry = 'INSERT INTO games (title, date, location, description) VALUES ($1, $2, $3, $4) RETURNING id';
   const values = [title, date, location, description];
   try {
-    const result = await pool.query(qry, values);
-    console.log(result);
-    return res.sendStatus(200);
+    const { rows } = await pool.query(qry, values);
+    return res.status(200).send(`Game added with id: ${rows[0].id}`);
   } catch(error) {
     console.log(error);
-    res.sendStatus(500);
+    res.status(500).send(`Error: could not add game. ${error}`);
   }
 });
 
@@ -69,16 +71,16 @@ app.get('/games', async (req, res) => {
   const qry = 'SELECT * FROM games';
   try {
     const { rows } = await pool.query(qry);
-    console.log(rows);
     return res.status(200).json(rows);
   } catch(error) {
     console.log(error);
-    res.sendStatus(500);
+    res.status(500).send(`Error: could not get games. ${error}`);
   }
 });
 
 app.get('/games/:id', async (req, res) => {
   const { id } = req.params;
+
   const qryGames = 'SELECT * FROM games WHERE id = $1';
   const qryPlayers = 'SELECT name, email, status FROM players WHERE game_id = $1';
   try {
@@ -93,37 +95,39 @@ app.get('/games/:id', async (req, res) => {
     return res.status(200).json(result);
   } catch(error) {
     console.log(error);
-    res.sendStatus(500);
+    res.status(500).send(`Error: could not get game. ${error}`);
   }
 });
 
 app.post('/games/:id/players', (req, res) => {
   const { name, email } = req.body;
   const { id } = req.params;
+  if(!name || !email) return res.status(400).send('Missing required fields');
+  if(!id) return res.status(400).send('Missing game id');
   const qry = 'INSERT INTO players (name, email, game_id) VALUES ($1, $2, $3)';
   const values = [name, email, id];
   try {
     const result = pool.query(qry, values);
-    console.log(result);
     return res.status(200).send('Player added to game');
   } catch(error) {
     console.log(error);
-    res.status(500).send('Error adding player to game');
+    res.status(500).send(`Error adding player to game. ${error}`);
   }
 });
 
 app.patch('/games/:id/players/:playerid', (req, res) => {
   const { playerid } = req.params;
   const { status } = req.body;
+  if(!playerid) return res.status(400).send('Missing player id');
+  if(!status) return res.status(400).send('Missing status');
   const qry = 'UPDATE players SET status = $1 WHERE id = $2';
   const values = [status, playerid];  
   try {
     const result = pool.query(qry, values);
-    console.log(result);
     return res.status(200).send('Player status updated');
   } catch(error) {
     console.log(error);
-    res.status(500).send('Error updating player status');
+    res.status(500).send(`Error updating player status. ${error}`);
   }
 });
 
